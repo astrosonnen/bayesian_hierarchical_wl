24a053d48fa322bce4ef22ff96efdabe9dc78cd0
Alessandro Sonnenfeld <sonnenfeld@strw.leidenuniv.nl>
2021-02-15 22:10:36 +0100

    added movie

---
+++ examples/frames/make_movie.sh
@@ -0,0 +1,1 @@
+convert -delay 15 -loop 0 *.png three_lenses_movie.gif
---
+++ examples/plot_three_halos_shapes.py
@@ -0,0 +1,114 @@
+import numpy as np
+import pylab
+import wl_lens_models
+from matplotlib.patches import Ellipse
+
+
+# draws the shapes of lensed sources around a lens
+nsource = 400
+
+fsize = 20
+
+# lens and source redshift (fixed for all)
+zd = 0.3
+zs = 1.0
+
+# three lenses
+m200 = [1e13, 1e12, 1e13] # halo masses
+
+# lens model: NFW halo + point mass at the center
+lens1 = wl_lens_models.NFWPoint(z=zd, m200=m200[0], mstar=1e11, c200=5.)
+lens2 = wl_lens_models.NFWPoint(z=zd, m200=m200[1], mstar=1e11, c200=5.)
+lens3 = wl_lens_models.NFWPoint(z=zd, m200=m200[2], mstar=1e11, c200=5.)
+lenses = [lens1, lens2, lens3]
+
+nlens = len(lenses)
+
+# lens positions
+xl = np.array([0., -0.1, 1.]) * lens1.Mpc2deg
+yl = np.array([0., 0.2, 0.]) * lens1.Mpc2deg
+
+# box half-size in degrees (16:9 aspect ratio to put in slide)
+xmax = 1.6*lens1.Mpc2deg # 1.6 Mpc 
+ymax = 0.9*lens1.Mpc2deg # 0.9 Mpc
+
+# draws source positions uniformly in a rectangle of size 2.*xmax x 2.*ymax
+x_source = 2.*xmax * (np.random.rand(nsource) - 0.5)
+y_source = 2.*ymax * (np.random.rand(nsource) - 0.5)
+
+# generates intrinsic shapes. Circular, if sigma_eps is set to 0.
+sigma_eps = 0. # 0.27
+eps11s = np.random.normal(0., sigma_eps, nsource)
+eps12s = np.random.normal(0., sigma_eps, nsource)
+
+kappa = np.zeros(nsource)
+gamma1 = np.zeros(nsource)
+gamma2 = np.zeros(nsource)
+
+# calculates the contribution of each lens to kappa and gamma
+for n in range(nlens):
+    # transforms to polar coordinates
+    r_source = ((x_source-xl[n])**2 + (y_source-yl[n])**2)**0.5
+    phi_source = np.arctan((y_source-yl[n])/(x_source-xl[n]))
+    phi_source[x_source<0.] = phi_source[x_source<0.] + np.pi
+    phi_source[phi_source<0.] += 2.*np.pi
+
+    # calculates amplitude and direction of the reduced shear at each source position
+    kappa += lenses[n].kappa(r_source, zs)
+    gamma1 += lenses[n].gamma1(r_source, phi_source, zs)
+    gamma2 += lenses[n].gamma2(r_source, phi_source, zs)
+
+gamma = (gamma1**2 + gamma2**2)**0.5
+
+gamma_pa = np.arctan(gamma2/gamma1)
+gamma_pa[gamma1<0.] = gamma_pa[gamma1<0.] + np.pi
+gamma_pa[gamma_pa<0.] += 2.*np.pi
+
+# lensing-induced ellipticity
+q = (1. - kappa - gamma)/(1. - kappa + gamma)
+crazy_q = 0.8*q # enhanced ellipticity
+
+ncirc = 101
+phi_circ = 2.*np.pi*np.linspace(0., 1., ncirc)
+ssize = 0.03*xmax
+r_circ = ssize * np.ones(ncirc)
+x_circ = r_circ * np.cos(phi_circ)
+y_circ = r_circ * np.sin(phi_circ)
+
+fig = pylab.figure(figsize=(16., 9.))
+ax = fig.add_subplot(111)#, aspect='equal')
+pylab.subplots_adjust(left=0., right=1., bottom=0., top=1.)
+
+for i in range(nsource):
+
+    ell = Ellipse(xy=(x_source[i], y_source[i]), width=ssize*crazy_q[i], height=ssize/crazy_q[i], angle=np.rad2deg(0.5*gamma_pa[i]))
+
+    ax.add_artist(ell)
+
+text_dx = -0.7*ssize
+text_dy = -0.4*ssize
+
+boxtext = ''
+for n in range(nlens):
+    ell = Ellipse(xy=(xl[n], yl[n]), width=2.*ssize, height=2.*ssize, angle=0., color='r')
+    ax.add_artist(ell)
+    ax.text(xl[n]+text_dx, yl[n]+text_dy, '$M_{%d}$'%(n+1), fontsize=fsize)
+    boxtext += '$M_{%d} = %2.1e\,M_{\odot}$\n'%(n+1, m200[n])
+boxtext = boxtext[:-1]
+
+props = dict(boxstyle='round', facecolor='wheat', alpha=1.)
+ax.text(-0.9*xmax, 0.7*ymax, boxtext, fontsize=fsize, bbox=props)
+ax.set_xlim(-xmax, xmax)
+ax.set_ylim(-ymax, ymax)
+
+# adds a ruler
+ax.errorbar(0.8*xmax, 0.8*ymax, yerr=0., xerr=0.1*lenses[0].Mpc2deg, capsize=5, color='k')
+ax.text(0.72*xmax, 0.82*ymax, '200 kpc', fontsize=fsize)
+
+pylab.xticks(())
+pylab.yticks(())
+pylab.axis('off')
+pylab.savefig('threelens_shapes_enhanced_circular_plot.png')
+pylab.show()
+
+
