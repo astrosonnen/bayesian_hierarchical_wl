95e0edcbebdec429f1a8d94a61ede3a1dc43b664
Alessandro Sonnenfeld <sonnenfeld@strw.leidenuniv.nl>
2021-05-07 16:10:48 +0200

    smarter way of calculating enclosed mass

--- wl_profiles/sersic.py
+++ wl_profiles/sersic.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.integrate import quad
 import os
-from scipy.special import gamma as gfunc
+from scipy.special import gamma as gfunc, gammainc
 from scipy.interpolate import splrep, splint
 import ndinterp
 import h5py
@@ -17,11 +17,7 @@ def Sigma(R, nser, Re):
     return np.exp(-b(nser)*(R/Re)**(1./nser))/L(nser, Re)
 
 def M2d(R, nser, Re):
-    R = np.atleast_1d(R)
-    out = 0.*R
-    for i in range(0, len(R)):
-        out[i] = 2*np.pi*quad(lambda r: r*Sigma(r, nser, Re), 0., R[i])[0]
-    return out
+    return 2.*np.pi*Re**2*nser/b(nser)**(2.*nser)*gammainc(2*nser, b(nser)*(R/Re)**(1./nser))/L(nser, Re)*gfunc(2.*nser)
 
 # grids for fast computation
 rgrid_min = 0.001
