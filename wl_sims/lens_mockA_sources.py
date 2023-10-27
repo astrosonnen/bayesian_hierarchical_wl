7f3e91d90ccbdfc2e7991f3b770c76c3bccfc5bb
Alessandro Sonnenfeld <alesonny@gmail.com>
2023-10-27 16:04:16 +0800

    added comments

--- wl_sims/lens_mockA_sources.py
+++ wl_sims/lens_mockA_sources.py
@@ -6,15 +6,17 @@ import h5py
 from cosmolopy import distance
 
 
+# this code calculates the observed ellipticity of the sources, after lensing
+
 mockname = 'mockAB'
 
 cosmo = {'omega_M_0': omegaM, 'omega_lambda_0': omegaL, 'omega_k_0': omegak, 'h': h}
 
 pop_file = h5py.File('mockAB_pop.hdf5', 'r')
 
-source_file = h5py.File('mockAB_sources_dsspline.hdf5', 'r')
+source_file = h5py.File('mockAB_sources.hdf5', 'r')
 
-lensed_source_file = h5py.File('mockA_lensed_sources_dsspline.hdf5', 'w')
+lensed_source_file = h5py.File('mockA_lensed_sources.hdf5', 'w')
 
 for par in ['zs_err', 'rmax', 'nbkg', 'sigma_eps']:
     lensed_source_file.create_dataset(par, data=source_file[par].value)
@@ -28,20 +30,26 @@ for i in range(nlens):
     lensname = 'lens_%05d'%i
     lensed_source_file.create_group(lensname)
 
+    # lens model Object: NFW + deVaucouleurs.
+    # Could probably use point-mass + NFW.
     lens = lens_tools.NFWdeVProfile(z=pop_file['zd'][i], m200=10.**pop_file['mhalo'][i], \
                                     c200=10.**pop_file['c200'][i], mstar=10.**pop_file['mstar'][i], reff=pop_file['reff'][i], ra=pop_file['ra'][i], \
                                     dec=pop_file['dec'][i])
 
+    # only lenses sources that are behind the lens
     bkg = source_file[lensname]['z'].value > pop_file['zd'][i] + 0.01
 
     nsource = len(source_file[lensname]['z'])
     nbkg = len(source_file[lensname]['z'][bkg])
 
+    # prepares the kappa and gamma arrays
     kappa_bkg = np.zeros(nbkg)
     gamma_bkg = 0.*kappa_bkg + 1j*0.*kappa_bkg
 
+    # initializes the complex ellipticity to the intrinsic value
     compell = source_file[lensname]['e1s'].value.copy() + 1j*source_file[lensname]['e2s'].value.copy()
 
+    # the lensing calculations are done by the lens model Object.
     bkg_sources = {}
 
     for par in ['s_cr', 'ra', 'dec']:
@@ -57,6 +65,9 @@ for i in range(nlens):
 
     gcompl_bkg = gamma_bkg / (1. - kappa_bkg)
 
+    # gets the observed ellipticity from the intrinsic one and 
+    # the reduced shear. It assumes |gcompl_bkg| < 1.
+    # (should probably be revised to account for strong lensing)
     compell[bkg] = (compell[bkg] + gcompl_bkg) / (1. + np.conj(compell[bkg])*np.conj(gcompl_bkg))
 
     e1 = np.real(compell)
@@ -65,7 +76,10 @@ for i in range(nlens):
     sin2phi = 2.*np.cos(source_file[lensname]['phi'])*np.sin(source_file[lensname]['phi'])
     cos2phi = np.cos(source_file[lensname]['phi'])**2 - np.sin(source_file[lensname]['phi'])**2
 
+    # tangential component of ellipticity
     et = e1*cos2phi + e2*sin2phi
+
+    # cross component
     er = -e1*sin2phi + e2*cos2phi
 
     for par in source_file[lensname]:
--- wl_sims/make_mockAB.py
+++ wl_sims/make_mockAB.py
@@ -6,7 +6,7 @@ import h5py
 from cosmolopy import distance
 
 
-mockname = 'mockABC'
+mockname = 'mockAB'
 
 cosmo = {'omega_M_0': omegaM, 'omega_lambda_0': omegaL, 'omega_k_0': omegak, 'h': h}
 
@@ -23,54 +23,71 @@ nz = 101
 
 sigma_eps = 0.27 # dispersion in intrinsic source shape
 
-rmax = 1.
+rmax = 1. # maximum radius (in Mpc) out to which sources are drawn
 
-mstar_mu = 11.
-mstar_sig = 0.4
+mstar_mu = 11. # average logM*
+mstar_sig = 0.4 # dispersion in logM*
 
-mstar_err = 0.15
+mstar_err = 0.15 # uncertainty on logM*
 
-mstar_cut = 11.
+mstar_cut = 11. # minimum cut on logM*(obs)
 
-mhalo_mu = 13.
-mhalo_sig = 0.4
-beta_h = 1.5
+mstar_piv = 11.2 # pivot stellar mass
 
-re_beta = 0.57
-re_mu = 0.54 + 0.25*re_beta
-re_sig = 0.16
-re_err = 0.04
+# Halo mass distribution: Gaussian in logMh, with mean
+# mu_h = mhalo_mu + beta_h * (mhalo_mu - mstar_piv)
+mhalo_mu = 13. # average logMh at the pivot stellar mass
+mhalo_sig = 0.4 # intrinsic scatter in logMh around the mean
+beta_h = 1.5 # halo mass-stellar mass correlation
 
-n0 = 1000000
+# size distribution
+re_beta = 0.57 # slope of mstar-Reff relation
+re_mu = 0.54 + 0.25*re_beta # average logRe at logM*=11 (after changing IMF)
+re_sig = 0.16 # intrinsic scatter in logRe
+re_err = 0.04 # uncertainty on logRe
 
-nlens = 5000
+n0 = 1000000 # large number for random sample drawing purposes
 
-dec_max = 45.
+nlens = 5000 # number of lenses
 
+dec_max = 45. # maximum declination (in case coordinates are needed)
+
+# first draws true stellar masses from a Gaussian
 mstar = np.random.normal(mstar_mu, mstar_sig, n0)
+
+# applies observational error
 mstar_obs = mstar + np.random.normal(0., mstar_err, n0)
 
+# applies cut on observed stellar mass
 sel = mstar_obs > mstar_cut
 
 indices = np.arange(n0)
-samp = np.random.choice(indices[sel], nlens)
+# only keeps the first nlens lenses
+samp = np.random.choice(indices[sel], nlens) 
 
 mstar = mstar[samp]
 mstar_obs = mstar_obs[samp]
 
-mhalo = mhalo_mu + beta_h * (mstar - 11.2) + np.random.normal(0., mhalo_sig, nlens)
+# draws halo masses
+mhalo = mhalo_mu + beta_h * (mstar - mstar_piv) + np.random.normal(0., mhalo_sig, nlens)
 
+# draws sizes
 reff = re_mu + re_beta * (mstar - 11.) + np.random.normal(0., re_sig, nlens)
+# adds observational error to sizes
 reff_obs = reff + np.random.normal(0., re_err, nlens)
 
+# draws concentration
 lc200 = c200_mu + c200_beta * (mhalo - c200_piv) + np.random.normal(0., c200_sig, nlens)
 
+# draws lens redshift (one-side truncated Gaussian)
 a, b = (zd_min - zd_mu)/zd_sig, (np.inf - zd_mu)/zd_sig
 zd = truncnorm.rvs(a, b, size=nlens)*zd_sig + zd_mu
 
+# draws coordinates
 ra = 360. * np.random.rand(nlens)
 dec = np.rad2deg(np.arcsin(np.sin(np.deg2rad(dec_max))*(np.random.rand(nlens)*2. - 1.)))
 
+# stores data
 hdf5_file = h5py.File(mockname+'_pop.hdf5', 'w')
 
 hyperpars = {'mstar_mu': mstar_mu, 'mstar_sig': mstar_sig, 'mstar_err': mstar_err, 'mstar_cut': mstar_cut, \
@@ -109,26 +126,32 @@ for i in range(nlens):
     Dd = distance.angular_diameter_distance(zd[i], **cosmo)
     rmax_deg = np.rad2deg(rmax/Dd)
     
-    area = pi*(rmax_deg*60.)**2  # area of background sources
-    nsource = np.random.poisson(area*nbkg)
+    area = pi*(rmax_deg*60.)**2  # area of circle with radius rmax
+    nsource = np.random.poisson(area*nbkg) # draws the number of sources
 
+    # draws source positions (uniform in the circle)
     r_source = (rmax_deg**2*np.random.rand(nsource))**0.5
     phi_source = 2.*pi*np.random.rand(nsource)
 
     ra_source = ra[i] - r_source*np.cos(phi_source)/np.cos(np.deg2rad(dec[i]))
     dec_source = dec[i] + r_source*np.sin(phi_source)
 
+    # draws redshifts
     a, b = -zs_mu/zs_sig, (5. - zs_mu)/zs_sig
     zs = truncnorm.rvs(a, b, size=nsource)*zs_sig + zs_mu
     
+    # adds errors to the redshifts
     zs_obs = zs + np.random.normal(0., zs_err, nsource)
     zs_obs[zs_obs < 0.] = 0.
    
+    # draws the intrinsic ellipticity: Gaussian with dispersion sigma_eps
     eps11s = np.random.normal(0., sigma_eps, nsource)
     eps12s = np.random.normal(0., sigma_eps, nsource)
 
+    # complex intrinsic ellipticity
     es = eps11s + 1j*eps12s
 
+    # calculates the critical density of each source
     s_cr = 0.*zs
     s_cr_obs = 0.*zs
 
@@ -173,10 +196,14 @@ for i in range(nlens):
     s_cr_err = s_cr_obs[obs_bkg] - s_cr_obs_up[obs_bkg]
     """
 
+    # propagates the redshift error to the critical density
     s_cr_obs_up = splev(zs_obs[obs_bkg] + zs_err, s_cr_spline)
     s_cr_err = s_cr_obs[obs_bkg] - s_cr_obs_up
 
+    # shape measurement error: 
+    # quadrature sum of intrinsic scatter
     shape_var = sigma_eps**2 * np.ones(nsource)
+    # and error on Sigma_cr
     shape_var[obs_bkg] += (s_cr_err/s_cr_obs[obs_bkg])**2
 
     sources = {}
